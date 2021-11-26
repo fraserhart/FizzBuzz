054715b81d8553d28fe16a2e00f8473a3a0e6235
Fraser Hart <fraser.hart@tesco.com>
2021-11-26 14:16:03 +0000

    revert to presolution

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,16 +1,3 @@
-const divisibleByThree = (num) => {
-  return num % 3 === 0;
-};
-
-const divisibleByFive = (num) => {
-  return num % 5 === 0;
-};
-
-const fizzBuzz = (num) => {
-  if (divisibleByThree(num) && divisibleByFive(num)) return `FizzBuzz`;
-  if (divisibleByThree(num)) return `Fizz`;
-  if (divisibleByFive(num)) return `Buzz`;
-  return num;
-};
+const fizzBuzz = (num) => {};
 
 module.exports = fizzBuzz;
